579ec4f0e370a2c9f02c371b8efe83a07eae4549
Andrea Catalina Fernández Mena <katy.fernandezmena@gmail.com>
2023-04-01 16:38:57 -0600

    MAIN Navegation Bar FrontEnd Only

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
-import { Navbar, Nav, Button,Container} from 'react-bootstrap';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import logoBtn from 'src/images/logoSearch.jpg';
 import styles from 'src/components/Menu.module.css';
-
-
-
 
 interface MenuProps {
   titulo: string;
@@ -15,21 +12,26 @@ interface MenuProps {
 const Menu = (props: MenuProps) => {
   return (
     <>
+      {/* Navbar component */}
       <Navbar className="shadow-lg">
         <Container>
-        <Navbar.Brand>
+          <Navbar.Brand>
+            {/* Image component */}
             <Image src={logoBtn} height={40}/>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mx-auto">
+              {/* Navigation links */}
               <Nav.Link className="container text-dark" style={{ fontSize:'17px',fontWeight: 'semi-bold' }}>Ordenes</Nav.Link>
               <Nav.Link className="container text-dark" style={{ fontSize:'17px',fontWeight: 'semi-bold'}}>Colaboradores</Nav.Link>
             </Nav>
+            {/* Button component */}
             <Button variant="container btn text-dark mr-5"className={styles['btn-custom']}>Mi cuenta</Button>
           </Navbar.Collapse>
         </Container>  
       </Navbar>
+      {/* Content */}
       <div className="container pt-4">
         <h1>{props.titulo}</h1>
         <p>{props.descripcion}</p>
