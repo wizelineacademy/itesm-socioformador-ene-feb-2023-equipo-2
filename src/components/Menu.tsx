c1b6704bfa2805070e132e6537acff258f0060ba
Fuentes2395 <afuentesalanis2002@gmail.com>
2023-04-10 16:08:34 -0600

    feature/LinksMenuAdminSinOrden

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import Image from 'next/image';
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
-import logoBtn from 'src/images/logoSearch.jpg';
-import styles from 'src/components/Menu.module.css';
+import React from "react";
+import Image from "next/image";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import Link from 'next/link';
+import logoBtn from "src/images/logoSearch.jpg";
+import styles from "src/components/Menu.module.css";
 
 interface MenuProps {
   titulo: string;
@@ -13,23 +14,76 @@ const Menu = (props: MenuProps) => {
   return (
     <>
       {/* Navbar component */}
-      <Navbar className="shadow-lg">
+      <Navbar className="border-bottom">
         <Container>
           <Navbar.Brand>
             {/* Image component */}
-            <Image src={logoBtn} height={40} alt="Logo Wizeline"/>
+            <Image src={logoBtn} height={40} alt="Logo Wizeline" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mx-auto">
               {/* Navigation links */}
-              <Nav.Link className="container text-dark" style={{ fontSize:'17px',fontWeight: 'semi-bold' }}>Ordenes</Nav.Link>
-              <Nav.Link className="container text-dark" style={{ fontSize:'17px',fontWeight: 'semi-bold'}}>Colaboradores</Nav.Link>
+              <Nav.Link
+                className="text-dark"
+                style={{
+                  fontSize: "17px",
+                  fontWeight: "semi-bold",
+                  marginRight: "20px",
+                }}
+              >
+                <Link href="/admin/clients">Clients</Link>
+              </Nav.Link>
+              <Nav.Link
+                className="text-dark"
+                style={{
+                  fontSize: "17px",
+                  fontWeight: "semi-bold",
+                  marginRight: "20px",
+                }}
+              >
+                <Link href="/admin/orders">Orders</Link>
+              </Nav.Link>
+              <Nav.Link
+                className="text-dark"
+                style={{
+                  fontSize: "17px",
+                  fontWeight: "semi-bold",
+                  marginRight: "20px",
+                }}
+              >
+                <Link href="/admin/teams">Teams</Link>
+              </Nav.Link>
+              <Nav.Link
+                className="text-dark"
+                style={{
+                  fontSize: "17px",
+                  fontWeight: "semi-bold",
+                  marginRight: "20px",
+                }}
+              >
+                <Link href="/admin/users">Users</Link>
+              </Nav.Link>
+              <Nav.Link
+                className="text-dark"
+                style={{
+                  fontSize: "17px",
+                  fontWeight: "semi-bold",
+                  marginRight: "20px",
+                }}
+              >
+                <Link href="/admin/skills">Skills</Link>
+              </Nav.Link>
             </Nav>
             {/* Button component */}
-            <Button variant="container btn text-dark mr-5"className={styles['btn-custom']}>Mi cuenta</Button>
+            <Button
+              variant="container btn text-dark mr-5"
+              className={styles["btn-custom"]}
+            >
+              Mi cuenta
+            </Button>
           </Navbar.Collapse>
-        </Container>  
+        </Container>
       </Navbar>
       {/* Content */}
       <div className="container pt-4">
