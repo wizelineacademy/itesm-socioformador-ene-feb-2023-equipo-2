efb9e96b055b73e8acce9c34c3ee28b553a3467a
Fuentes2395 <afuentesalanis2002@gmail.com>
2023-05-16 09:57:14 -0600

    update-hello-world

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
 @app.route("/cv-linkedin", methods=["POST"])
 def index_cv():
   try: 
