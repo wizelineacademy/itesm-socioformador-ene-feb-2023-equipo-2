74d907ec2e81911537c9140058cd239de2f9ac03
Jorge de Leon <jorge.deleon@lernit.com.mx>
2023-05-17 21:08:17 -0600

    implementing the auto-generation auth0 token

--- next.config.js
+++ next.config.js
@@ -4,3 +4,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      'superagent-proxy': false
+    };
+
+    return config;
+  },
+};
