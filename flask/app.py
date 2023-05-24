d70f20c9aaa1903f3a6b1969c1fd52a947c223ab
Oscar A Reyna Monreal <A01570385@tec.mx>
2023-05-24 14:07:26 -0600

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -24,14 +24,7 @@ def index_cv():
     link_linkedin = request.json["link_linkedin"]
     profile_linkedin = ""
     #--------------------------------------------------------
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("enable-automation")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
     driver.get("https://linkedin.com/uas/login")
     time.sleep(5)
 
