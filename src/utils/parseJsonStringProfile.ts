cbe907e97f57fa6ded9ef521cdec551984101695
Oscar A Reyna Monreal <A01570385@tec.mx>
2023-05-31 08:18:55 -0600

    Update to profile and adding function to preview employee info

---
+++ src/utils/parseJsonStringProfile.ts
@@ -0,0 +1,41 @@
+export function parseJsonStringProfile(string: any) {
+    
+    console.log("string raw", string?.infoabout)
+    
+    //removing breakpoints and "/" characters
+    let cleanString = string.replace(/\\n|\\r|\//gm, "");
+
+    const replacements = {
+        'à': 'a', 'á': 'a', 'â': 'a', 'ã': 'a', 'ä': 'a', 'å': 'a',
+        'è': 'e', 'é': 'e', 'ê': 'e', 'ë': 'e',
+        'ì': 'i', 'í': 'i', 'î': 'i', 'ï': 'i',
+        'ñ': 'n',
+        'ò': 'o', 'ó': 'o', 'ô': 'o', 'õ': 'o', 'ö': 'o',
+        'ù': 'u', 'ú': 'u', 'û': 'u', 'ü': 'u', 'ý': 'y', 'ÿ': 'y'
+      };
+    
+    const replaceAccentedCharacters = (text: string): string => {
+        const pattern = /[àáâãäåèéêëìíîïñòóôõöùúûüýÿ]/g;
+        return text.replace(pattern, (match) => replacements[match] || match);
+    };
+
+    cleanString = replaceAccentedCharacters(cleanString);
+
+    //removing non printable characters
+    cleanString = cleanString.replace(/[^\x20-\x7E]/g, '');
+    
+    //cleanString = cleanString.replace(/\S/g, '');
+    cleanString = cleanString.slice(1, cleanString.length-1);
+    cleanString = JSON.stringify(cleanString);
+
+    console.log("string cleaned", cleanString);
+
+    let parsedJson = JSON.parse(cleanString);
+
+    let finalJson = JSON.parse(parsedJson);
+
+    return finalJson;
+
+    
+      
+}
