d033323e5b6edc782a33908e98828476dc0332db
Fuentes2395 <afuentesalanis2002@gmail.com>
2023-06-06 11:13:20 -0600

    create-client.ts update test

--- cypress/components/a01177466_AndresFuentes/ClientCreation.cy.js
+++ cypress/components/a01177466_AndresFuentes/ClientCreation.cy.js
@@ -27,29 +27,29 @@ describe('ClientCreation Component', () => {
   //   cy.get("button").contains("Add").should("exist");
   // });
 
-  // it('should display success message after adding a client', () => {
-  //   cy.intercept('POST', '/api/create-client', (req) => {
-  //     req.reply({
-  //       statusCode: 201,
-  //       body: {
-  //         id: 1, // You can generate a unique ID or use a predefined value
-  //         name: 'John Doe',
-  //         email: 'johndoe@example.com',
-  //         phone: '1234567890',
-  //         erased: false,
-  //       },
-  //     });
-  //   }).as('createClient');
+  it('should display success message after adding a client', () => {
+    cy.intercept('POST', '/api/create-client', (req) => {
+      req.reply({
+        statusCode: 201,
+        body: {
+          id: 1, // You can generate a unique ID or use a predefined value
+          name: 'John Doe',
+          email: 'johndoe@example.com',
+          phone: '1234567890',
+          erased: false,
+        },
+      });
+    }).as('createClient');
 
-  //   cy.get('input#name').type('John Doe');
-  //   cy.get('input#email').type('johndoe@example.com');
-  //   cy.get('input#phone').type('1234567890');
-  //   cy.get('button').contains('Add').click();
+    cy.get('input#name').type('John Doe');
+    cy.get('input#email').type('johndoe@example.com');
+    cy.get('input#phone').type('1234567890');
+    cy.get('button').contains('Add').click();
 
-  //   cy.wait('@createClient').then(() => {
-  //     cy.contains('Client added successfully').should('be.visible');
-  //   });
-  // });
+    cy.wait('@createClient').then(() => {
+      cy.contains('Client added successfully').should('be.visible');
+    });
+  });
 
   // it("displays missing field message when a field is left empty", () => {
   //   cy.get('input#name').type('John Doe');
